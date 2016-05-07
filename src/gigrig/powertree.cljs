26ff4f8314c897e98fad67034e4ab2d873dc10a1
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-05-08 00:20:20 +0200

    Use custom zipper to represent powertree

--- src/gigrig/powertree.cljs
+++ src/gigrig/powertree.cljs
@@ -1,8 +1,46 @@
-(ns gigrig.powertree)
+(ns gigrig.powertree
+  (:require [clojure.zip :as zip :refer [up down left right]]))
 
+;; Zipper
+;; ======
+(defn- branch? [node]
+  (or
+   (= :distributor (first node))
+   (= :isolator (first node))))
+
+(defn- children [node]
+  (rest node))
+
+(defn- make-node [node children]
+  (cons (first node) children))
+
+(defn- zipper
+  ([] (zipper :distributor))
+  ([root] (zip/zipper branch? children make-node [root])))
+
+(defn location-type [loc]
+  (-> loc zip/node first))
+
+
+;; Tree
+;; ====
+(def DISTRIBUTOR_LIMIT 5)
+
+(defn insert [loc pedal]
+  (if (<= (count (zip/children loc)) DISTRIBUTOR_LIMIT)
+    (zip/append-child loc pedal)
+    (let [new-node (zip/make-node loc [:distributor] [(last (zip/children loc)) pedal])]
+      (-> loc
+          zip/down
+          zip/rightmost
+          (zip/replace new-node)))))
+
+;; Powertree
+;; =========
 (defn- simplify
   "returns a hashmap containing the prefered type of the power supply"
   [pedal]
-  {:type (-> (filter val pedal)
-             keys
-             last)})
+  {:name (:name pedal)
+   :power (-> (filter val pedal)
+              keys
+              last)})
--- test/gigrig/powertree_test.cljs
+++ test/gigrig/powertree_test.cljs
@@ -1,7 +1,52 @@
 (ns gigrig.powertree-test
   (:require [cljs.test :refer-macros [deftest is testing]]
+            [clojure.zip :as zip]
             [gigrig.powertree :as ptree]))
 
+(defn echo [arg]
+  (.log js/console arg)
+  arg)
+
+(defn distributor-pedal [n]
+  {:type :pedal
+   :name (str "pedal" n)
+   :distributor true})
+
+(defn isolator-pedal [n]
+  {:type :pedal
+   :name (str "pedal" n)
+   :distributor true
+   :isolator true})
+
 (deftest simplify
-  (is (= {:type :distributor} (ptree/simplify {:distributor true :isolator false})))
-  (is (= {:type :isolator} (ptree/simplify {:distributor true :isolator true}))))
+  (is (= {:power :distributor :name "pedal1"} (ptree/simplify (distributor-pedal 1))))
+  (is (= {:power :isolator :name "pedal2"} (ptree/simplify (isolator-pedal 2)))))
+
+(defn eql-zip [expected loc]
+  (= expected (zip/root loc)))
+
+(deftest insert
+  (let [root (ptree/zipper)]
+    (is (eql-zip [:distributor [:pedal "pedal1"]]
+                 (ptree/insert root [:pedal "pedal1"])))
+    (is (eql-zip [:distributor [:pedal "pedal1"] [:pedal "pedal2"] [:pedal "pedal3"] [:pedal "pedal4"] [:pedal "pedal5"] [:pedal "pedal6"]]
+                 (-> root
+                     (ptree/insert [:pedal "pedal1"])
+                     (ptree/insert [:pedal "pedal2"])
+                     (ptree/insert [:pedal "pedal3"])
+                     (ptree/insert [:pedal "pedal4"])
+                     (ptree/insert [:pedal "pedal5"])
+                     (ptree/insert [:pedal "pedal6"]))))
+    (is (eql-zip [:distributor [:pedal "pedal1"] [:pedal "pedal2"] [:pedal "pedal3"] [:pedal "pedal4"] [:pedal "pedal5"] [:distributor [:pedal "pedal6"] [:pedal "pedal7"]]]
+                 (-> root
+                     (ptree/insert [:pedal "pedal1"])
+                     (ptree/insert [:pedal "pedal2"])
+                     (ptree/insert [:pedal "pedal3"])
+                     (ptree/insert [:pedal "pedal4"])
+                     (ptree/insert [:pedal "pedal5"])
+                     (ptree/insert [:pedal "pedal6"])
+                     (ptree/insert [:pedal "pedal7"]))))
+    (is (eql-zip [:distributor [:pedal "pedal1"] [:isolator [:pedal "pedal2"]]]
+                 (-> root
+                     (ptree/insert [:pedal "pedal1"])
+                     (ptree/insert [:isolator [:pedal "pedal2"]]))))))
