c7c466980853955e3d8d0dd189d1ff43809bc507
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-05-08 23:04:33 +0200

    Fix selectors and tests to reflect real app-state

--- src/gigrig/selectors.cljs
+++ src/gigrig/selectors.cljs
@@ -3,6 +3,4 @@
 (defn selected-pedals
   "Returns a sequence of pedals found in searchfields as selected"
   [state]
-  (->> (:searchfields state)
-       (map #(-> % vals first :pedal))
-       (filter (complement nil?))))
+  (filter (complement nil?) (map :pedal (-> state :search-fields vals))))
--- test/gigrig/selectors_test.cljs
+++ test/gigrig/selectors_test.cljs
@@ -5,10 +5,9 @@
 (deftest selected-pedals
   (is (= ["pedal1" "pedal2"]
          (s/selected-pedals
-          {:searchfields [{:g__1 {:pedal "pedal1"}}
-                          {:g__2 {:pedal "pedal2"}}]})))
+          {:search-fields {:g__1 {:pedal "pedal1"}
+                           :g__2 {:pedal "pedal2"}}})))
   (is (= ["pedal2"]
          (s/selected-pedals
-          {:searchfields [{:g__1 {:pedal nil}}
-                          {:g__2 {:pedal "pedal2"}}]}))))
-
+          {:search-fields {:g__1 {:pedal nil}
+                           :g__2 {:pedal "pedal2"}}}))))
