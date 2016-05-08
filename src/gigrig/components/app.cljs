1b9d16f1908cfbbfbd70c583c31e8811382c716b
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-05-08 20:16:57 +0200

    Hook up zipper and diagram

--- src/gigrig/components/app.cljs
+++ src/gigrig/components/app.cljs
@@ -1,22 +1,15 @@
 (ns gigrig.components.app
   (:require [gigrig.actions :as a]
             [redux.core :refer [dispatch!]]
-            [gigrig.zipper :as gzip]
+            [gigrig.powertree :as ptree]
             [gigrig.components.diagram :as diagram]
             [gigrig.components.pedal-search :as pedal-search]))
 
-(def test-tree
-  [:distributor
-   [[:pedal "pedal1"]
-    [:pedal "pedal2"]
-    [:pedal "pedal3"]
-    [:pedal "pedal4"]
-    [:pedal "pedal5"]
-    [:distributor [[:pedal "pedal 6"]
-                   [:pedal "pedal 7"]
-                   [:isolator [[:pedal "pedal 8"]]]
-                   [:distributor [[:pedal "pedal 9"]
-                                  [:pedal "pedal 10"]]]]]]])
+(def test-pedals
+  [{:brand "Ibanez" :model "AD9 Analog Delay" :distributor true :isolator true :other false}
+   {:brand "Ibanez" :model "TubeScreamer" :distributor true :isolator false :other false}
+   {:brand "Ibanez" :model "Whammy" :distributor true :isolator false :other false}
+   {:brand "Ibanez" :model "BlueSky" :distributor true :isolator true :other false}])
 
 (defn component
   [state]
@@ -26,4 +19,4 @@
      ^{:key id}
      [pedal-search/component (assoc field :id id)])
    [:button {:on-click #(dispatch! a/add-pedal-button-clicked)} "+ Add pedal"]
-   [diagram/component {:zipper (gzip/zipper test-tree)}]])
+   [diagram/component {:zipper (ptree/build test-pedals)}]])
