a2b818ae4cfec0e4cf376f1d424673850809d02e
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-05-15 08:21:53 +0200

    Style dropdown

--- src/gigrig/components/pedal_search.cljs
+++ src/gigrig/components/pedal_search.cljs
@@ -12,22 +12,23 @@
                                :on-change #(handle-change (.. % -target -value))}])
 
 (defn- suggestion [props]
-  [:li {:on-mouse-down #(dispatch! a/pedal-selected (:id props))}
-   [:strong (:model props)]
-   [:span (:brand props)]])
+  [:li.suggestion {:on-mouse-down #(dispatch! a/pedal-selected (:id props))}
+   [:strong.suggestion__model (:model props)]
+   [:span.suggestion__brand (:brand props)]])
 
 (defn dropdown [props]
-  [:ul
+  [:ul.suggestions-dropdown
    (for [s (:suggestions props)]
      ^{:key (:id s)} [suggestion s])])
 
 (defn component []
   (let [dropdown-visible (reagent/atom false)]
     (fn [props]
-      [:div.pedal-search
-       [search-field {:on-focus #(do (reset! dropdown-visible true) nil)
-                      :on-blur #(do (reset! dropdown-visible false) nil)
-                      :value (:query props)
-                      :handle-change (fn [val] (dispatch! a/searchfield-key-pressed val))}]
-       (if @dropdown-visible
-         [dropdown {:suggestions (:suggestions props)}])])))
+      [:div.pedal-search-outer
+       [:div.pedal-search
+        [search-field {:on-focus #(do (reset! dropdown-visible true) nil)
+                       :on-blur #(do (reset! dropdown-visible false) nil)
+                       :value (:query props)
+                       :handle-change (fn [val] (dispatch! a/searchfield-key-pressed val))}]
+        (if @dropdown-visible
+          [dropdown {:suggestions (:suggestions props)}])]])))
