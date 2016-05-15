8b898101ba829a8cf81c32ee7d90f84b3f81946c
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-05-15 07:53:19 +0200

    Style and markup pedals that can't be rendered + use sass

--- src/gigrig/components/selected_pedals.cljs
+++ src/gigrig/components/selected_pedals.cljs
@@ -1,9 +1,16 @@
 (ns gigrig.components.selected-pedals
   (:require [redux.core :refer [dispatch!]]
+            [gigrig.powertree :as ptree]
             [gigrig.actions :as a]))
 
+(defn- in-diagram?
+  "Wether or not the pedal has sufficient data to be rendered in the
+  diagram"
+  [pedal]
+  (some #(% pedal) [:adapter :isolator :distributor]))
+
 (defn pedal [{:keys [pedal id]}]
-  [:div.selected-pedal
+  [:div {:class (if (in-diagram? pedal) "selected-pedal" "selected-pedal--red")}
    [:span (:model pedal)]
    [:button.selected-pedal__remove {:on-click #(dispatch! a/pedal-removed id)}]])
 
